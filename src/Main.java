6e12f7633d698d85d02af63a62a98d843e86915c
DingZhenyuan <ryandingzy@gmail.com>
2022-01-28 19:27:42 -0800

    amazon prime air route

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        solution.testGetMinTime();
+//        solution.testGetMinTime();
+        solution.testGetPair();
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -28,5 +29,50 @@ public class Solution {
         System.out.println(getMinTime(lst.size(), lst));
     }
 
+    // Amazon Prime Air Route
+    public int[] getPair(int max, int[][] forward, int[][] backward) {
+        Arrays.sort(forward, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        Arrays.sort(backward, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        int f = 0, b = backward.length - 1;
+        int maxSum = 0;
+        int[] res = new int[2];
+        while (f < forward.length && b >= 0) {
+            int sum = forward[f][1] + backward[b][1];
+            if (sum > max) {
+                b--;
+                continue;
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                res = new int[]{forward[f][0], backward[b][0]};
+            }
+            f++;
+        }
+        return res;
+    }
 
+    public void testGetPair() {
+        int[][] forward = new int[][]{
+                {1, 3000},
+                {2, 5000},
+                {3, 4000},
+                {4, 10000}
+        };
+        int[][] backward = new int[][]{
+                {1, 2000},
+                {2, 3000},
+                {3, 4000}
+        };
+        int[] res = getPair(11000, forward, backward);
+        System.out.println(res[0] + "-" + res[1]);
+    }
 }
